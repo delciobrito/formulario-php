6ce8e0815755780de4e8184ec1bb1b0060ba0df8
Delcio Brito <delciobrito@gmail.com>
2022-11-10 21:23:09 -0300

    Adicionando funções de calculaMedia e situacaoAluno

--- calcular.php
+++ calcular.php
@@ -5,11 +5,31 @@ $notaAv2 = $_GET['f_av2'];
 
 echo "<p>Nome do aluno: $name</p>";
 echo "<p>Nota AV1: $notaAv1</p>";
-echo "<p>Nota AV2: $notaAv1</p>";
+echo "<p>Nota AV2: $notaAv2</p>";
 
+/*
 if(($notaAv1 + $notaAv2)/2 >= 6){
     echo "Situação: APROVADO";
 } else{
     echo "Situação: ALUNO EM AV3"; 
 }
+*/
+
+function calculaMedia($n1, $n2){
+    $media = ($n1 + $n2) / 2;
+    echo "<p>Média: $media</p>";
+    return $media;
+}
+
+function situacaoAluno($media){
+    if($media >= 6){
+        echo "<p>Situação: APROVADO</p>";
+    } else {
+        echo "<p>Situação: Aluno em AV3</p>";
+    }
+}
+
+$media = calculaMedia($notaAv1, $notaAv2);
+situacaoAluno($media);
+
 ?>
