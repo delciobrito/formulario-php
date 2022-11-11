cdcb32962f0b2184ec386d3f0c3e85698144eb9f
Delcio Brito <delciobrito@gmail.com>
2022-11-10 21:06:37 -0300

    Revert "Adicionando função media"
    This reverts commit ec551a727bfe28fcaad757439462d73cee804243.

--- calcular.php
+++ calcular.php
@@ -7,23 +7,9 @@ echo "<p>Nome do aluno: $name</p>";
 echo "<p>Nota AV1: $notaAv1</p>";
 echo "<p>Nota AV2: $notaAv1</p>";
 
-/*
 if(($notaAv1 + $notaAv2)/2 >= 6){
     echo "Situação: APROVADO";
 } else{
     echo "Situação: ALUNO EM AV3"; 
 }
-*/
-
-function media($a, $b){
-    $media = ($a + $b) / 2;
-    return $media;
-}
-
-if(media($notaAv1, $notaAv2) >= 6){
-    echo "Situação: APROVADO";
-} else {
-    echo "Situação: Aluno na AV3";
-}
-
 ?>
